b035d1a73b4df39ff789b618fcbb25ae86840419
hessamasna <hessamasna@yahoo.com>
2021-08-29 15:46:20 +0430

    [UPT] routers in app-routing.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {DashboardComponent} from "./dashboard/dashboard.component";
+import {PipelinesComponent} from "./dashboard/pipelines/pipelines.component";
+import {DatasetsComponent} from "./dashboard/datasets/datasets.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'dashboard', component:DashboardComponent, children:[
+      {path:'pipelines', component:PipelinesComponent},
+      {path:'datasets', component:DatasetsComponent},
+    ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
