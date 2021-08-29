feaca7fb62e4324a04df9e1cf4b0dae83e672f2f
hessamasna <hessamasna@yahoo.com>
2021-08-29 15:55:53 +0430

    [FIX] routers

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,9 +1,9 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { DashboardComponent } from './dashboard.component';
-import { PipelinesComponent } from './pipelines/pipelines.component';
-import { DatasetsComponent } from './datasets/datasets.component';
-
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {DashboardComponent} from './dashboard.component';
+import {PipelinesComponent} from './pipelines/pipelines.component';
+import {DatasetsComponent} from './datasets/datasets.component';
+import {RouterModule} from "@angular/router";
 
 
 @NgModule({
@@ -13,7 +13,12 @@ import { DatasetsComponent } from './datasets/datasets.component';
     DatasetsComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
+  ],
+  exports:[
+    DashboardComponent,
   ]
 })
-export class DashboardModule { }
+export class DashboardModule {
+}
