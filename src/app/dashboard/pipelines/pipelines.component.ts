c7e3e96483df61a1e4c382fc75eabd7d0281d3e7
hessamasna <hessamasna@yahoo.com>
2021-08-29 15:36:00 +0430

    [ADD] pipelines component

---
+++ src/app/dashboard/pipelines/pipelines.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pipelines',
+  templateUrl: './pipelines.component.html',
+  styleUrls: ['./pipelines.component.scss']
+})
+export class PipelinesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
