6657be577ee398a26d2dadfe940a39cf59063a8d
ParsaArvanehPA <parsa.arvaneh@gmail.com>
2021-08-30 00:06:17 +0430

    [MOD] routing for dataset

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {DatasetsComponent} from './pages/datasets/datasets.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path: 'datasets', component: DatasetsComponent}];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
