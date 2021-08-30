99a8ad68428335d2a4609e2e802568877051e821
hessamasna <hessamasna@yahoo.com>
2021-08-30 13:32:58 +0430

    [DEL] Toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,10 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {DashboardModule} from "./pages/dashboard/dashboard.module";
-import {ToolbarComponent} from "./components/toolbar/toolbar.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToolbarComponent
   ],
   imports: [
     BrowserModule,
