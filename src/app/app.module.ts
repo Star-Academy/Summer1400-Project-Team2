639d01b55725416689cab1601c47170490f134f3
hadis ghafouri <hadisghafouri98@gmail.com>
2021-08-30 14:00:31 +0430

    [ADD] pipeline-header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PipelineHeaderComponent } from './components/pipeline-header/pipeline-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PipelineHeaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/pipeline-header/pipeline-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pipeline-header',
+  templateUrl: './pipeline-header.component.html',
+  styleUrls: ['./pipeline-header.component.scss']
+})
+export class PipelineHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
