9171296e7090c81645a7106b810387ae8fb54eda
ParsaArvanehPA <parsa.arvaneh@gmail.com>
2021-08-30 20:04:48 +0430

    [ADD] chart.js & ng2-chart

--- src/app/pages/datasets/datasets.module.ts
+++ src/app/pages/datasets/datasets.module.ts
@@ -1,13 +1,22 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {DatasetsComponent} from './datasets.component';
-import { SideMenuComponent } from './side-menu/side-menu.component';
-import { TopMenuComponent } from './top-menu/top-menu.component';
-import { OverviewComponent } from './overview/overview.component';
-import { SampleComponent } from './sample/sample.component';
+import {SideMenuComponent} from './side-menu/side-menu.component';
+import {TopMenuComponent} from './top-menu/top-menu.component';
+import {OverviewComponent} from './overview/overview.component';
+import {SampleComponent} from './sample/sample.component';
+import {RadarChartComponent} from './overview/radar-chart/radar-chart.component';
+import {ChartsModule} from 'ng2-charts';
 
 @NgModule({
-    declarations: [DatasetsComponent, SideMenuComponent, TopMenuComponent, OverviewComponent, SampleComponent],
-    imports: [CommonModule],
+    declarations: [
+        DatasetsComponent,
+        SideMenuComponent,
+        TopMenuComponent,
+        OverviewComponent,
+        SampleComponent,
+        RadarChartComponent,
+    ],
+    imports: [CommonModule, ChartsModule],
 })
 export class DatasetsModule {}
