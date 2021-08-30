3a861c0778e8546963cf75feb2e05366679965f4
ParsaArvanehPA <parsa.arvaneh@gmail.com>
2021-08-30 14:32:16 +0430

    [ADD] handled section changin in dataser

--- src/app/pages/datasets/datasets.component.ts
+++ src/app/pages/datasets/datasets.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-datasets',
-  templateUrl: './datasets.component.html',
-  styleUrls: ['./datasets.component.scss']
+    selector: 'app-datasets',
+    templateUrl: './datasets.component.html',
+    styleUrls: ['./datasets.component.scss'],
 })
 export class DatasetsComponent implements OnInit {
+    layout: string = 'overview';
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {}
 
+    layoutChanger(layout: any) {
+        this.layout = layout;
+    }
 }
