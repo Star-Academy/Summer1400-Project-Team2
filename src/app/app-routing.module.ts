86d28d85a2f0a3d4b2bc97ef921e8058651240a4
hessamasna <hessamasna@yahoo.com>
2021-08-30 10:46:25 +0430

    [UPT] folders

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {DashboardComponent} from "./dashboard/dashboard.component";
-import {PipelinesComponent} from "./dashboard/pipelines/pipelines.component";
-import {DatasetsComponent} from "./dashboard/datasets/datasets.component";
+import {DashboardComponent} from "./pages/dashboard/dashboard.component";
+import {PipelinesComponent} from "./pages/dashboard/pipelines/pipelines.component";
+import {DatasetsComponent} from "./pages/dashboard/datasets/datasets.component";
 
 const routes: Routes = [
   {path:'dashboard', component:DashboardComponent, children:[
