8b3ecc38a4a7ed0931aa1721710e27d28d2b977a
hadis ghafouri <hadisghafouri98@gmail.com>
2021-08-31 12:42:39 +0430

    [ADD] tooltip to buttons

--- src/app/pages/pipeline/pipeline.module.ts
+++ src/app/pages/pipeline/pipeline.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { PipelinePage } from './pipeline.component';
 import { PipelineRoutingModule } from './pipeline-routing.module';
 import { PipelineHeaderComponent } from './pipeline-header/pipeline-header.component';
+import {MatTooltipModule} from '@angular/material/tooltip';
 @NgModule({
   declarations: [PipelinePage, PipelineHeaderComponent],
-  imports: [CommonModule, PipelineRoutingModule],
+  imports: [CommonModule, PipelineRoutingModule,MatTooltipModule],
   exports:[PipelinePage]
 })
 export class PipelineModule {}
