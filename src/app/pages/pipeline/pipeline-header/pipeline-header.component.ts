6810185e26792f68c68b3fed9bdfab0b7f8bb201
hadis ghafouri <hadisghafouri98@gmail.com>
2021-08-31 12:17:44 +0430

    [ADD] method to buttons in header component

--- src/app/pages/pipeline/pipeline-header/pipeline-header.component.ts
+++ src/app/pages/pipeline/pipeline-header/pipeline-header.component.ts
@@ -6,10 +6,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pipeline-header.component.scss']
 })
 export class PipelineHeaderComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
+  hasClicked = false;
 
-  ngOnInit(): void {
+  onEditBtn() {
+    this.hasClicked = true;
+    this.toggleEdit();
+  }
+  toggleEdit() {
+    const pipelineInfo = <HTMLElement>document.querySelector('.pipeline-info');
+    const changeContainer = <HTMLElement>document.querySelector('.change-pipeline__container');
+    if (this.hasClicked) {
+      pipelineInfo.style.display = 'none';
+      changeContainer.style.display = 'flex';
+    } else {
+      pipelineInfo.style.display = 'flex';
+      changeContainer.style.display = 'none';
+    }
+  }
+
+  onCancelBtn() {
+    this.hasClicked = false;
+    this.toggleEdit();
+  }
+  onSubmitBtn() {
+    this.hasClicked = false;
+    this.toggleEdit();
+  }
+  onBackBtn(){
+    window.history.back();
   }
 
 }
