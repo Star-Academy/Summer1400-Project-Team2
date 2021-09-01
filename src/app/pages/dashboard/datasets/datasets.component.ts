3cc13b4347f0346812e26de770c1b0690f11b934
hessamasna <hessamasna@yahoo.com>
2021-09-01 19:04:43 +0430

    [ADD] table and buttons

--- src/app/pages/dashboard/datasets/datasets.component.ts
+++ src/app/pages/dashboard/datasets/datasets.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+export interface DatasetData {
+  id: number;
+  name: string;
+  connectionName: string;
+  created: string;
+  star: boolean;
+}
+
+const DATA: DatasetData[] = [
+  { id: 1, name: 'داده فیک', connectionName: 'دمو', created: 'امروز', star: true },
+  { id: 2, name: 'داده فیک2', connectionName: 'دمو', created: 'ماه پیش', star: false }
+];
 
 @Component({
   selector: 'app-datasets',
   templateUrl: './datasets.component.html',
   styleUrls: ['./datasets.component.scss']
 })
-export class DatasetsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class DatasetsComponent {
+  rowClick(row: unknown): void {
+    console.log(row);
   }
 
+  displayedColumns: string[] = ['name', 'connectionName', 'created', 'star'];
+  dataSource = DATA;
 }
