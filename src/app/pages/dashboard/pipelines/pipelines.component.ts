a708bdb33d47309cdb1508856f26a29576ed458c
hessamasna <hessamasna@yahoo.com>
2021-09-01 17:56:46 +0430

    [ADD] table and buttons

--- src/app/pages/dashboard/pipelines/pipelines.component.ts
+++ src/app/pages/dashboard/pipelines/pipelines.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+export interface PipelineData {
+  id: number;
+  name: string;
+  connectionName: string;
+  created: string;
+  star: boolean;
+}
+
+const DATA: PipelineData[] = [
+  { id: 1, name: 'داده فیک', connectionName: 'دمو', created: 'امروز', star: true },
+  { id: 2, name: 'داده فیک2', connectionName: 'دمو', created: 'ماه پیش', star: false }
+];
 
 @Component({
   selector: 'app-pipelines',
   templateUrl: './pipelines.component.html',
   styleUrls: ['./pipelines.component.scss']
 })
-export class PipelinesComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class PipelinesComponent {
+  rowClick(row: unknown): void {
+    console.log(row);
   }
 
+  files: File[] = [];
+
+  onSelect(event: { addedFiles: File }): void {
+    this.files.push(event.addedFiles);
+  }
+
+  onRemove(event: File): void {
+    this.files.splice(this.files.indexOf(event), 1);
+  }
+
+  displayedColumns: string[] = ['name', 'connectionName', 'created', 'star'];
+  dataSource = DATA;
 }
