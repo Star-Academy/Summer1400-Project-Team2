95b4dbb755b97eec27d3bf86c5c8088c7b160d1f
hessamasna <hessamasna@yahoo.com>
2021-09-01 17:34:19 +0430

    [UPT] style

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent {
+  innerWidth: number;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.innerWidth = window.screen.width;
   }
 
+  public width(): boolean {
+    return innerWidth >= 480;
+  }
 }
