0a43f32acb7399c3629ea7691d9cac38277e79df
Soroush Taslimi <soroushtaslimi@yahoo.com>
2021-09-03 19:29:14 +0430

    add NodeService

---
+++ ETL-project-Team2/ETL-project-Team2/services/NodeService.cs
@@ -0,0 +1,20 @@
+﻿using ETL_project_Team2.models;
+
+namespace ETL_project_Team2.services
+{
+    public class NodeService
+    {
+        private static NodeService _nodeServiceInstance;
+
+        private NodeService(){}
+        public static NodeService GetInstance()
+        {
+            return _nodeServiceInstance ??= new NodeService();
+        }
+
+        public void Operate(Node node)
+        {
+            node.NodeOperation.Operate();
+        }
+    }
+}
