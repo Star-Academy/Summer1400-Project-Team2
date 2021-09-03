e000dcd2261dbc804912648a956f7762071cd85b
hessamasna <hessamasna@yahoo.com>
2021-09-03 14:07:31 +0430

    [UPT] 404 page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,19 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PipelineModule } from './pages/pipeline/pipeline.module';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [AppComponent, NotFoundComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, PipelineModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    PipelineModule,
+    MatButtonModule,
+    MatIconModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
