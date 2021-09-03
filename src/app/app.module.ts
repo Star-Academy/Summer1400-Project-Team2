550aceb0796dd7405336344c62ba781da735b22d
hessamasna <hessamasna@yahoo.com>
2021-09-03 13:39:07 +0430

    [ADD] 404 page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PipelineModule } from './pages/pipeline/pipeline.module';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NotFoundComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, PipelineModule],
   providers: [],
   bootstrap: [AppComponent]
