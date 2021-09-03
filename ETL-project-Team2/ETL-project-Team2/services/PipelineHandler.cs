d37a7048c099babf84d8a45b4d90e556bb5baccd
Soroush Taslimi <soroushtaslimi@yahoo.com>
2021-09-03 19:29:44 +0430

    add PipelineHandler

---
+++ ETL-project-Team2/ETL-project-Team2/services/PipelineHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+using ETL_project_Team2.models;
+
+namespace ETL_project_Team2.services
+{
+    public class PipelineHandler
+    {
+        public List<Node> Pipeline { get; set; }
+        public string Name { get; set; }
+        public SqlConnection EntryDbConnection { get; set; }
+        public SqlConnection FinalDbConnection { get; set; }
+        public string FilePath { get; set; }
+        
+        public void OperatePipeline(){
+            foreach (Node node in Pipeline)
+            {
+                NodeService.GetInstance().Operate(node);
+            }
+        }
+    }
+    
+}
