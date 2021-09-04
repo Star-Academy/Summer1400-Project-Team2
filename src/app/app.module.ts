921e3c82121a3732ccf381b7477a19be52742da9
hessamasna <hessamasna@yahoo.com>
2021-09-04 22:17:49 +0430

    [UPT] modules and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,11 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PipelineModule } from './pages/pipeline/pipeline.module';
 import { DatasetsModule } from './pages/datasets/datasets.module';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, LoginComponent, RegisterComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
