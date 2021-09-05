93c6aeb5c629bf3d6017490cd6833454df953863
hessamasna <hessamasna@yahoo.com>
2021-09-05 19:56:36 +0430

    [UPT] register

--- src/app/pages/register/register.module.ts
+++ src/app/pages/register/register.module.ts
@@ -1,9 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegisterComponent } from './register.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [RegisterComponent],
-  imports: [CommonModule]
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    RouterModule
+  ]
 })
 export class RegisterModule {}
