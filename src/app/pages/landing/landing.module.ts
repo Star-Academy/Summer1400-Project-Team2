4442acf579df12bc079766056db1e6928e8f86a4
ParsaArvanehPA <parsa.arvaneh@gmail.com>
2021-09-05 22:40:17 +0430

    [ADD] landing module

---
+++ src/app/pages/landing/landing.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LandingComponent } from './landing.component';
+
+@NgModule({
+  declarations: [LandingComponent],
+  imports: [CommonModule]
+})
+export class LandingModule {}
