702a50c9c71e6c004bca632b2d89947b90216b7c
hessamasna <hessamasna@yahoo.com>
2021-09-05 13:05:36 +0430

    [UPT] login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,18 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PipelineModule } from './pages/pipeline/pipeline.module';
 import { DatasetsModule } from './pages/datasets/datasets.module';
-import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { LoginModule } from './pages/login/login.module';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, RegisterComponent],
+  declarations: [AppComponent, RegisterComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     PipelineModule,
-    DatasetsModule
+    DatasetsModule,
+    LoginModule
   ],
   providers: [],
   bootstrap: [AppComponent]
