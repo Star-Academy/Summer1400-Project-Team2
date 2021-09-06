b16ff621fb2f0780f773ad791a7c5f9e68ff092f
Amirhosein Abedi <73402569+Mir-Abedi@users.noreply.github.com>
2021-09-06 15:55:11 +0430

    added dbservice to filterservice

--- ETL-project-Team2/ETL-project-Team2/services/FilterService.cs
+++ ETL-project-Team2/ETL-project-Team2/services/FilterService.cs
@@ -6,17 +6,20 @@ namespace ETL_project_Team2.services
     public class FilterService : IFilterService
     {
         private readonly IYmlFilterParser _ymlFilterParser;
+        private readonly IDBService _dbService;
         
-        public FilterService(IYmlFilterParser ymlFilterParser)
+        public FilterService(IYmlFilterParser ymlFilterParser, IDBService dbService)
         {
             this._ymlFilterParser = ymlFilterParser;
+            _dbService = dbService;
         }
         
         public void Filter(string query, string currentDbName, string destinationDbName, SqlConnection sqlConnection)
         {
             var command = MakeQuery(query, currentDbName, destinationDbName);
-            var sqlCommand = new SqlCommand(command, sqlConnection);
-            sqlCommand.ExecuteNonQuery();
+            // var sqlCommand = new SqlCommand(command, sqlConnection);
+            // sqlCommand.ExecuteNonQuery();
+            _dbService.ExecuteNonQuery()
         }
 
         private string MakeQuery(string query, string currentDbName, string destinationDbName)
