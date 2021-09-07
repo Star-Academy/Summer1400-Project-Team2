b43b87a21c670d9af618ab6f7bbdcb34a7e018b5
hessamasna <hessamasna@yahoo.com>
2021-09-07 16:28:10 +0430

    dasd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {DashboardModule} from "./pages/dashboard/dashboard.module";
-import {MatDialog, MatDialogModule} from '@angular/material/dialog';
+import { DashboardModule } from './pages/dashboard/dashboard.module';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { PipelineModule } from './pages/pipeline/pipeline.module';
 import { FormsModule } from '@angular/forms';
 import { DatasetsModule } from './pages/datasets/datasets.module';
 import { LoginModule } from './pages/login/login.module';
 import { RegisterModule } from './pages/register/register.module';
-
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NotFoundComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -30,4 +30,4 @@ import { RegisterModule } from './pages/register/register.module';
   providers: [MatDialog],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
