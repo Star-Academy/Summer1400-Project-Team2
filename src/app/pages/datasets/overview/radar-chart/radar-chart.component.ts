88bfa38256134f559c56db2c08bcfb0bc18cad3d
ParsaArvanehPA <parsa.arvaneh@gmail.com>
2021-09-09 00:31:58 +0430

    [MOD] re-design radar chart

--- src/app/pages/datasets/overview/radar-chart/radar-chart.component.ts
+++ src/app/pages/datasets/overview/radar-chart/radar-chart.component.ts
@@ -1,42 +1,51 @@
-import {Component, OnInit} from '@angular/core';
-import {ChartDataSets, ChartType, RadialChartOptions} from 'chart.js';
-import {Label} from 'ng2-charts';
+import { Component, OnInit } from '@angular/core';
+import { ChartDataSets, ChartType, RadialChartOptions } from 'chart.js';
+import { Label } from 'ng2-charts';
 
 @Component({
-    selector: 'app-radar-chart',
-    templateUrl: './radar-chart.component.html',
-    styleUrls: ['./radar-chart.component.scss'],
+  selector: 'app-radar-chart',
+  templateUrl: './radar-chart.component.html',
+  styleUrls: ['./radar-chart.component.scss']
 })
-export class RadarChartComponent implements OnInit {
-    constructor() {}
+export class RadarChartComponent {
+  public radarChartOptions: RadialChartOptions = {
+    responsive: true,
+    maintainAspectRatio: false,
+    legend: {
+      display: false
+    },
+    scale: {
+      ticks: {
+        maxTicksLimit: 4,
+        //TODO
+        display: false
+      },
+      gridLines: {
+        //TODO
+        // display: false,
+        color: 'hsl(6, 100%, 61%, .5)'
+      },
 
-    public radarChartOptions: RadialChartOptions = {
-        responsive: true,
-        maintainAspectRatio: false,
-        legend: {
-            display: false,
-        },
-        scale: {
-            ticks: {
-                maxTicksLimit: 4,
-                //TODO
-                display: false,
-            },
-            gridLines: {
-                //TODO
-                // display: false,
-            },
+      pointLabels: {
+        fontSize: 14
+      }
+    }
+  };
 
-            pointLabels: {
-                fontSize: 14,
-            },
-        },
-    };
+  public radarChartLabels: Label[] = [
+    'اعتبار',
+    'محبوبیت',
+    'کامل بودن',
+    'قابل کشف',
+    'میزان کارکرد'
+  ];
 
-    public radarChartLabels: Label[] = ['اعتبار', 'محبوبیت', 'کامل بودن', 'قابل کشف', 'میزان کارکرد'];
-
-    public radarChartData: ChartDataSets[] = [{data: [85, 68, 60, 38, 86]}];
-    public radarChartType: ChartType = 'radar';
-
-    ngOnInit(): void {}
+  public radarChartData: ChartDataSets[] = [
+    {
+      data: [85, 68, 60, 38, 86],
+      backgroundColor: 'hsl(236, 55%, 48%, .5)',
+      borderColor: 'hsl(236, 55%, 48%)'
+    }
+  ];
+  public radarChartType: ChartType = 'radar';
 }
