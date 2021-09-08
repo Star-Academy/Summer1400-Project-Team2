392f0803ddd3b7376302d4d7ea0a58203343787b
SamanEN <SamanEslami78@gmail.com>
2021-09-08 13:16:36 +0430

    updated Node model

--- ETL-project-Team2/ETL-project-Team2/models/Node.cs
+++ ETL-project-Team2/ETL-project-Team2/models/Node.cs
@@ -4,14 +4,9 @@ namespace ETL_project_Team2.models
 {
     public class Node
     {
+        public int Id { get; set; }
         public double XAxis { get; set; }
         public double YAxis { get; set; }
         public SqlTable TempTable { get; set; }
-
-        public void UpdateCoordinates(double x, double y)
-        {
-            XAxis = x;
-            YAxis = y;
-        }
     }
 }
