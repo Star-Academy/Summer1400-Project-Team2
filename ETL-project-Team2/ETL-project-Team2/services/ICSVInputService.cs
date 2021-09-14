cda6fa2ec295b0991bc28ce3c6485cdef425fe3b
SamanEN <SamanEslami78@gmail.com>
2021-09-14 12:29:47 +0430

    added new method to csvInputHandler interface

--- ETL-project-Team2/ETL-project-Team2/services/ICSVInputService.cs
+++ ETL-project-Team2/ETL-project-Team2/services/ICSVInputService.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.SqlClient;
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
 using ETL_project_Team2.models;
 
 namespace ETL_project_Team2.services
@@ -6,6 +7,7 @@ namespace ETL_project_Team2.services
     public interface ICSVInputService
     {
         void GetDataTableFromCsvFile(string filePath, string delimeter);
+        Dictionary<string, string> GetCSVColumns(string filePath, string delimeter);
         void ImportDataToSql(SqlTable table);
     }
 }
