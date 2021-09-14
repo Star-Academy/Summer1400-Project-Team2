fb8bdcc35ff7a4ae776c9fd2d7ca3587bab10c30
Amirhosein Abedi <73402569+Mir-Abedi@users.noreply.github.com>
2021-09-14 18:12:53 +0430

    Update DBAccessor.cs

--- ETL-project-Team2/ETL-project-Team2/dao/DBAccessor.cs
+++ ETL-project-Team2/ETL-project-Team2/dao/DBAccessor.cs
@@ -1,5 +1,6 @@
 ﻿using ETL_project_Team2.models;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,13 +10,32 @@ namespace ETL_project_Team2.dao
 {
     public class DBAccessor : IDBAccessor
     {
-        public int ExecuteNonQuery(string queryCommand, SqlConnection dbConnection)
+        private static readonly ConcurrentBag<KeyValuePair<string, SqlCommand>> Commands =
+            new ConcurrentBag<KeyValuePair<string, SqlCommand>>();
+        public int ExecuteNonQuery(string cancellationToken, string queryCommand, SqlConnection dbConnection)
         {
             using (var sqlCommand = new SqlCommand(queryCommand, dbConnection))
             {
+                var keyValuePair = new KeyValuePair<string, SqlCommand>(cancellationToken, sqlCommand);
+                Commands.Add(keyValuePair);
                 dbConnection.Open();
-                return sqlCommand.ExecuteNonQuery();
+                var temp = sqlCommand.ExecuteNonQuery();
+                Commands.TryTake(out keyValuePair);
+                return temp;
             }
+        }
+
+        public void CancelExecution(string cancellationToken)
+        {
+            foreach (var pair in Commands)
+            {
+                if (pair.Key != cancellationToken) continue;
+                pair.Value.Cancel();
+                
+                return;
+            }
+
+            throw new Exception("SqlCommand Already finished Executing.");
         }
     }
 }
