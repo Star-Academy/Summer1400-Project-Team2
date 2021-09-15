6473605a8fc5bbdccabed158cc8adbbd3c62aa48
hadis ghafouri <hadisghafouri98@gmail.com>
2021-09-15 10:56:41 +0430

    remove extra codes

--- src/app/pages/pipeline/processor-modal/processor-modal.component.ts
+++ src/app/pages/pipeline/processor-modal/processor-modal.component.ts
@@ -1,58 +1,35 @@
 import { Component, OnInit, Inject, Optional } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { PipelinePage } from '../pipeline.component';
-import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { MatDialog} from '@angular/material/dialog';
 
-export interface UsersData {
-  name: string;
-  id: number;
-}
 @Component({
   selector: 'app-processor-modal',
   templateUrl: './processor-modal.component.html',
   styleUrls: ['./processor-modal.component.scss']
 })
 export class ProcessorModalComponent implements OnInit {
-  chosenProcessor = '';
-  local_data: any;
   action = '';
   constructor(
     public _dialog: MatDialog,
     public dialogRef: MatDialogRef<PipelinePage>,
     @Optional() @Inject(MAT_DIALOG_DATA) public data: any
-  ) {
-    console.log(data);
-    this.local_data = { ...data };
-    this.action = this.local_data.action;
-  }
-
-  public openDialog(processor: any) {
-    const dialogConfig = new MatDialogConfig();
-    dialogConfig.disableClose = true;
-    dialogConfig.autoFocus = true;
-    dialogConfig.data = {
-      processor: processor
-    };
-    this._dialog.open(ProcessorModalComponent, dialogConfig);
-  }
+  ) {}
 
   ngOnInit(): void {}
   onFilterBtn() {
     this.action = 'filter';
     this.onClose();
-    // this.dialogRef.close();
   }
   onAggregateBtn() {
     this.action = 'aggregate';
     this.onClose();
-    // this.dialogRef.close();
   }
   onJoinBtn() {
     this.action = 'join';
     this.onClose();
-    // this.dialogRef.close();
   }
   onClose() {
-    this.dialogRef.close({ event: this.action, data: this.local_data });
+    this.dialogRef.close({ event: this.action });
   }
 }
