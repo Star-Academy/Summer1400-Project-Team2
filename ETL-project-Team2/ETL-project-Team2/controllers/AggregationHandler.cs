a7dd82a0b2892cf543fb4278c6883e51e3c216c6
Amirhosein Abedi <73402569+Mir-Abedi@users.noreply.github.com>
2021-09-15 19:16:05 +0430

    finished controller

--- ETL-project-Team2/ETL-project-Team2/controllers/AggregationHandler.cs
+++ ETL-project-Team2/ETL-project-Team2/controllers/AggregationHandler.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Text.Json;
 using ETL_project_Team2.controllers;
+using ETL_project_Team2.dao;
 using ETL_project_Team2.models;
 using ETL_project_Team2.services;
 
@@ -9,16 +10,25 @@ namespace Aggregation.Controllers
     public class AggregationHandler : IAggregationHandler
     {
         private readonly AggregationService _aggregationService;
+        private readonly IDBAccessor _dbAccessor;
         private AggregationModel _aggregationModel;
 
-        public AggregationHandler(AggregationService aggregationService)
+        public AggregationHandler(AggregationService aggregationService, IDBAccessor dbAccessor)
         {
             _aggregationService = aggregationService;
+            _dbAccessor = dbAccessor;
         }
 
         public SqlTable Operate(SqlTable table)
         {
-            throw new NotImplementedException();
+            _aggregationModel.SqlTable = new SqlTable()
+            {
+                DBConnection = table.DBConnection,
+                Coloumns = table.Coloumns
+            };
+            _aggregationModel.AggregateQuery = _aggregationService.CreateQuery(table, _aggregationModel);
+            _dbAccessor.ExecuteNonQuery("", _aggregationModel.AggregateQuery, _aggregationModel.SqlTable.DBConnection);
+            return _aggregationModel.SqlTable;
         }
 
         public void SetParameters(string jsonString)
