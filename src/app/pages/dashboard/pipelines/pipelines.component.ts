c00c11264891e38a78790755247673e28b1f2def
hessamasna <hessamasna@yahoo.com>
2021-09-15 18:25:25 +0430

    [UPT] work on services

--- src/app/pages/dashboard/pipelines/pipelines.component.ts
+++ src/app/pages/dashboard/pipelines/pipelines.component.ts
@@ -1,34 +1,36 @@
-import { Component, AfterViewInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {DashboardService} from "../../../services/dashboard/dashboard.service";
+import {tableData} from "../tableData";
+import {Router} from "@angular/router";
 
-interface Data {
-  name: string,
-  modelId: number
-}
 
-let res: any;
-
-let DATA: Data[] = [
-  { name: 'فیک', modelId: 1},
-  { name: 'فیک2', modelId: 2},
-];
 @Component({
   selector: 'app-pipelines',
   templateUrl: './pipelines.component.html',
   styleUrls: ['./pipelines.component.scss']
 })
-export class PipelinesComponent implements AfterViewInit {
-  constructor(private dashboardService: DashboardService) {
-  }
-  ngAfterViewInit() {
-    res = this.dashboardService.tableData('pipelines');
-    DATA = res;
+export class PipelinesComponent implements OnInit {
+
+  constructor(private dashboardService: DashboardService, private router:Router) {
   }
 
-  rowClick(row: unknown): void {
-    console.log(row);
+  table: tableData[] = [{name:'1',id:'2'}];
+
+  ngOnInit() {
+    this.dashboardService.tableData1('pipeline').subscribe(data => {
+      this.table = data;
+    })
   }
 
-  displayedColumns: string[] = ['modelId','name'];
-  dataSource = DATA;
+  rowClick(row: tableData): void {
+    // console.log(row);
+    this.router.navigateByUrl('/pipelines/'+ row.id);
+  }
+
+  create(id: string, name: string, entryDB: string, finalDB: string): void {
+    this.dashboardService.create(id, name, entryDB, finalDB);
+  }
+
+  displayedColumns: string[] = ['id', 'name'];
+   dataSource = this.table;
 }
